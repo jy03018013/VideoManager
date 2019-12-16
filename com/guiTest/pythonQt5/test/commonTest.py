0b80515cec46f83117ad303db4be57bc92f63c46
gxc <419975897@qq.com>
2019-12-17 00:47:00 +0800

    0046

--- com/guiTest/pythonQt5/test/commonTest.py
+++ com/guiTest/pythonQt5/test/commonTest.py
@@ -1,8 +1,7 @@
+import requests
 
-path = 'I:/丝袜，着衣系列/DPMX\\2\\Dpmx-012.mp4'
-name = 'DPMX\\2\\Dpmx-012'
-
-
-
-
-print(name.replace("\\","/"))
+url='http://www.h28o.com/cn/vl_searchbyid.php?keyword=IPX-316'
+r = requests.get(url) #发get请求
+print(r)#返回<Response [200]>
+# print(r.json())#将返回的json串转为字典
+print(r.text)#返回get到的页面的返回数据
